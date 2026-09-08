40fceebbf14d56d257801040a739890682ba4dce
Damar Arief W <damarariefwtjksn@gmail.com>
2026-09-08 14:38:04 +0700

    cp01 page 01

--- app/src/main/java/com/example/kantinku/MainActivity.java
+++ app/src/main/java/com/example/kantinku/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.kantinku;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 1. Deklarasi variabel Button
+    private Button btnMulaiPesan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // 2. Hubungkan variabel dengan ID Button di XML
+        btnMulaiPesan = findViewById(R.id.button);
+
+        // 3. Tambahkan fungsi klik untuk berpindah halaman
+        btnMulaiPesan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
         });
     }
 }
